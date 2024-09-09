fa315487521f6df7bc2308186d053a71493c71c4
Patrick Mota <patrickmota22@gmail.com>
2024-09-08 21:54:09 -0300

    feat: separate logic into CustomCalendar and add new features
    - extracted functionality from  calendar into CustomCalendar for better modularization
    - implements the marking of dates that have tasks
    - fix image aspect ratio in header
    - makes some changes to the task component to display the information

--- app/(tabs)/calendar/index.tsx
+++ app/(tabs)/calendar/index.tsx
@@ -1,79 +1,160 @@
 import { ScreenContainer } from '@/components/ScreenContainer';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 import { useState } from 'react';
-import { Calendar, LocaleConfig } from 'react-native-calendars';
-import { DateData, Theme } from 'react-native-calendars/src/types';
-import styled, { useTheme } from 'styled-components/native';
+import { LocaleConfig } from 'react-native-calendars';
+import { DateData } from 'react-native-calendars/src/types';
 
 import { ptBR } from '@/utils/localeConfigCalendar';
-import { Text } from 'react-native';
+import { FlatList, View } from 'react-native';
+import { DayTitle, EmptyListText } from './styles';
+import { Task } from '@/components/Task';
+import { CustomCalendar } from '@/components/CustomCalendar';
 
 LocaleConfig.locales['pt-br'] = ptBR;
 
 LocaleConfig.defaultLocale = 'pt-br';
 
 export default function TabTwoScreen() {
-  const theme = useTheme();
+  const today = new Date();
 
   const [day, setDay] = useState<DateData>({
-    dateString: new Date().toLocaleDateString().replace(/\//g, '-').split('-').reverse().join('-'),
-    day: new Date().getDate(),
-    month: new Date().getMonth(),
-    timestamp: new Date().getTime(),
-    year: new Date().getFullYear(),
+    dateString: today.toLocaleDateString().replace(/\//g, '-').split('-').reverse().join('-'),
+    day: today.getDate(),
+    month: today.getMonth(),
+    timestamp: today.getTime(),
+    year: today.getFullYear(),
   });
-
-  const calendarTheme: Theme = {
-    calendarBackground: 'transparent',
-    dayTextColor: theme.onPrimaryFixedVariant,
-    textMonthFontFamily: 'Montserrat-SemiBold',
-    textDayHeaderFontSize: 12,
-    monthTextColor: theme.onBackground,
-    arrowColor: theme.onBackground,
-    todayTextColor: theme.secondary,
-    selectedDayBackgroundColor: theme.secondary,
-    selectedDayTextColor: theme.onSecondary,
-    arrowStyle: {
-      margin: 0,
-      padding: 0,
-    },
-  };
 
   return (
     <ScreenContainer>
-      <CustomCalendar
-        theme={calendarTheme}
-        renderArrow={(direction: 'right' | 'left') => {
-          return (
-            <MaterialCommunityIcons
-              name={`chevron-${direction}`}
-              size={24}
-              color={theme.onBackground}
-            />
-          );
-        }}
-        onDayPress={setDay}
-        markedDates={
-          day && {
-            [day.dateString]: {
-              selected: true,
-            },
-          }
-        }
-        hideExtraDays
-      />
-      <Text style={{ color: theme.onBackground }}>
+      <CustomCalendar day={day} setDay={setDay} tasks={tasks} />
+      <DayTitle>
         {ptBR.dayNames[new Date(day?.timestamp!).getUTCDay()!]} -{' '}
         {day?.day.toString().padStart(2, '0')} de{' '}
         {ptBR.monthNames[new Date(day?.timestamp!).getUTCMonth()!]} de {day?.year}
-      </Text>
+      </DayTitle>
+      <FlatList
+        data={tasks.filter((task) => task.date === day.dateString)}
+        style={{ flexShrink: 0, marginBottom: 250 }}
+        keyExtractor={(item) => item.title}
+        contentContainerStyle={{ gap: 24 }}
+        renderItem={({ item }) => <Task {...item} time={item.time || ''} showTime />}
+        ListFooterComponent={() => <View style={{ height: 40 }}></View>}
+        ListEmptyComponent={<EmptyListText>Nenhuma tarefa para este dia</EmptyListText>}
+      />
     </ScreenContainer>
   );
 }
 
-const CustomCalendar = styled(Calendar)`
-  font-family: 'Montserrat-Regular';
-  width: 100%;
-  background-color: transparent;
-  padding: 0;
-`;
+const tasks = [
+  {
+    title: 'Fazer comida',
+    date: '2024-09-09',
+    time: '11:30',
+    tag: 'Casa',
+    description: 'Preparar macarrão à bolonhesa',
+    done: false,
+  },
+  {
+    title: 'Estudar JavaScript',
+    date: '2024-09-08',
+    time: '15:00',
+    tag: 'Estudo',
+    description: 'Revisar closures e promises',
+    done: false,
+  },
+  {
+    title: 'Estudar Redux',
+    date: '2024-09-08',
+    time: '19:00',
+    tag: 'Estudo',
+    description: 'Revisar closures e promises',
+    done: false,
+  },
+  {
+    title: 'Tomar Café',
+    date: '2024-09-08',
+    time: '18:00',
+    tag: 'Estudo',
+    description: 'Revisar closures e promises',
+    done: false,
+  },
+  {
+    title: 'Estudar API',
+    date: '2024-09-08',
+    time: '20:00',
+    tag: 'Estudo',
+    description: 'Revisar closures e promises',
+    done: false,
+  },
+  {
+    title: 'Ler livro',
+    date: '2024-09-08',
+    time: '21:30',
+    tag: 'Estudo',
+    description: 'Revisar closures e promises',
+    done: false,
+  },
+  {
+    title: 'Comprar mantimentos',
+    date: '2024-09-10',
+    time: '09:00',
+    tag: 'Compras',
+    description: 'Comprar arroz, feijão e frutas',
+    done: true,
+  },
+  {
+    title: 'Reunião com a equipe',
+    date: '2024-09-26',
+    time: '14:00',
+    tag: 'Trabalho',
+    description: 'Revisar o progresso do projeto',
+    done: false,
+  },
+  {
+    title: 'Exercícios físicos',
+    date: '2024-09-08',
+    tag: 'Saúde',
+    description: 'Correr no parque',
+    done: true,
+  },
+  {
+    title: 'Estudar React Native',
+    date: '2024-09-08',
+    time: '15:00',
+    tag: 'Estudo',
+    description: 'Revisar hooks e context API',
+    done: false,
+  },
+  {
+    title: 'Comprar ração para o cachorro',
+    date: '2024-09-10',
+    time: '09:00',
+    tag: 'Compras',
+    description: 'Comprar ração e petiscos',
+    done: true,
+  },
+  {
+    title: 'Reunião com o cliente',
+    date: '2024-10-03',
+    time: '14:00',
+    tag: 'Trabalho',
+    description: 'Apresentar o protótipo do aplicativo',
+    done: false,
+  },
+  {
+    title: 'Alongamento',
+    date: '2024-09-08',
+    tag: 'Saúde',
+    description: 'Alongar os músculos',
+    done: true,
+  },
+  {
+    title: 'Fazer café da manhã',
+    date: '2024-09-09',
+    time: '07:30',
+    tag: 'Casa',
+    description: 'Preparar café e pão com manteiga',
+    done: false,
+  },
+];
---
+++ components/CustomCalendar/styles.ts
@@ -0,0 +1,9 @@
+import { Calendar } from 'react-native-calendars';
+import styled from 'styled-components/native';
+
+export const StyledCalendar = styled(Calendar)`
+  font-family: 'Montserrat-Regular';
+  width: 100%;
+  background-color: transparent;
+  padding: 0;
+`;
