dd61a9a1aa89225ed52cbbe12873a850d6855042
Yasmin Miranda <mirandayasminym@gmail.com>
2024-10-03 22:29:42 -0300

    feat: implement new-tesk screen

--- app/new-task/index.tsx
+++ app/new-task/index.tsx
@@ -39,8 +39,8 @@ export default function NewTask() {
           />
 
           <Row>
-            <DatetimePicker mode="date" handleData={handleChange('date')} />
-            <DatetimePicker mode="time" handleData={handleChange('time')} />
+            <DatetimePicker mode="date" value={values.date} handleData={handleChange('date')} />
+            <DatetimePicker mode="time" value={values.time} handleData={handleChange('time')} />
           </Row>
           <TextArea
             value={values.description}
