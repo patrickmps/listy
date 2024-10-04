ad775fdde9543c3d2775b30bf57a1fecfe3c65b3
Yasmin Miranda <mirandayasminym@gmail.com>
2024-10-03 21:55:49 -0300

    feat: implement new-tesk screen

--- app/new-task/index.tsx
+++ app/new-task/index.tsx
@@ -1,21 +1,23 @@
 import { FormEvent } from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import { Btn, Container, Row, TextBtn } from './styles';
+import { Btn, Container, Row, TextBtn, TextArea, TextTitle } from './styles';
 import { DatetimePicker } from '@/components/DatetimePicker';
-
+import { Stack, useLocalSearchParams } from 'expo-router';
 const validationSchema = Yup.object().shape({
   date: Yup.string(),
   time: Yup.string(),
   tag: Yup.string(),
   description: Yup.string(),
-  // title: Yup.string().required('Campo obrigatório'),
+  title: Yup.string().required('Campo obrigatório'),
 });
 
 export default function NewTask() {
+  const params = useLocalSearchParams();
+
   const initialValues = {
     date: '',
-    time: '18:00',
+    time: '',
     tag: '',
     description: '',
     title: '',
@@ -26,13 +28,26 @@ export default function NewTask() {
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={(values) => console.log(values)}>
-      {({ handleChange, handleSubmit, values }) => (
+      {({ handleChange, handleSubmit, values, handleBlur }) => (
         <Container>
-          <Row>
-            <DatetimePicker mode="date" value={values.date} handleData={handleChange('date')} />
-            <DatetimePicker mode="time" value={values.time} handleData={handleChange('time')} />
-          </Row>
+          <Stack.Screen options={{ title: (params.name as string) || 'Nova Tarefa' }} />
+          <TextTitle
+            value={values.title}
+            onChangeText={handleChange('title')}
+            placeholder="Título da tarefa"
+            onBlur={handleBlur('title')}
+          />
 
+          <Row>
+            <DatetimePicker mode="date" handleData={handleChange('date')} />
+            <DatetimePicker mode="time" handleData={handleChange('time')} />
+          </Row>
+          <TextArea
+            value={values.description}
+            onChangeText={handleChange('description')}
+            placeholder="Descrição da tarefa"
+            onBlur={handleBlur('description')}
+          />
           <Btn onPress={(e) => handleSubmit(e as unknown as FormEvent<HTMLFormElement>)}>
             <TextBtn> Salvar tarefa</TextBtn>
           </Btn>
