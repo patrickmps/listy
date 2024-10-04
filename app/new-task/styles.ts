040cbb4853556d251de2f654debdd4ac871b641b
Yasmin Miranda <mirandayasminym@gmail.com>
2024-10-03 21:55:49 -0300

    feat: implement new-tesk screen

--- app/new-task/styles.ts
+++ app/new-task/styles.ts
@@ -45,6 +45,37 @@ export const TextArea = styled(TextInput).attrs(({ theme }) => ({
   color: ${({ theme }) => theme.onBackground};
 `;
 
+export const TextTitle = styled(TextInput).attrs({
+  multiline: true,
+  textAlignVertical: 'top',
+})`
+  padding: 4px;
+  color: ${({ theme }) => theme.onPrimaryContainer};
+  height: 56px;
+  text-align-vertical: top;
+  margintop: 10px;
+  flex-wrap: wrap;
+  font-family: 'Montserrat-Regular';
+  font-weight: 600;
+  font-size: 20px;
+  color: ${({ theme }) => theme.onPrimaryContainer};
+`;
+
+export const TextArea = styled(TextInput).attrs({
+  multiline: true,
+  textAlignVertical: 'top',
+})`
+  flex: 1;
+  padding: 8px;
+  height: 150px;
+  text-align-vertical: top;
+  margintop: 10px;
+  flex-wrap: wrap;
+  font-size: 16px;
+  font-family: 'Montserrat-Regular';
+  color: ${({ theme }) => theme.onBackground};
+`;
+
 export const Btn = styled.TouchableOpacity`
   background-color: ${({ theme }) => theme.palettes.primary[80]};
   color: ${({ theme }) => theme.onPrimary};
